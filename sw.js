7236cc4565fb8afe2da7826e9f57789a28a0b77e
mahamadoukdiarra <mahamadoukdiarra04@gmail.com>
2026-05-21 11:29:30 +0000

    Serve legacy iPhone start URL without redirect

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gecaf-inv-v11";
+const CACHE_NAME = "gecaf-inv-v12";
 const PAGE_FALLBACK = "./";
 const ASSETS = [PAGE_FALLBACK, "./styles.css", "./app.js", "./supabase-config.js", "./manifest.webmanifest"];
 
